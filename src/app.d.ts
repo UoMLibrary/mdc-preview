0e5a65351bf5730db901342e168e0bb484a8e27d
Christopher Wilson <chris@abitofcode.com>
2026-05-23 20:15:53 +0100

    chore: add TypeScript project baseline

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,25 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
+	interface SaxonTransformConfig {
+		sourceText?: string;
+		destination?: 'serialized' | string;
+		stylesheetInternal?: unknown;
+		[key: string]: unknown;
+	}
+
+	interface SaxonTransformResult {
+		principalResult: string;
+		[key: string]: unknown;
+	}
+
+	interface SaxonJSRuntime {
+		transform(config: SaxonTransformConfig, executionMode: 'async'): Promise<SaxonTransformResult>;
+		transform(config: SaxonTransformConfig, executionMode?: 'sync'): SaxonTransformResult;
+	}
+
+	const SaxonJS: SaxonJSRuntime;
+
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
