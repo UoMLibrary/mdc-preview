4f7318e8ce8f8b8a72501ff478e01075a4ec0af3
Christopher Wilson <chris@abitofcode.com>
2026-05-24 13:49:01 +0100

    fix: use JavaScript Svelte config for Docker builds

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-node';
-import type { Config } from '@sveltejs/kit';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const config: Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
 		adapter: adapter()
 	},
